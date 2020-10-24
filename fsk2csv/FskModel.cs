f33aa1b6df7ee90eabfa548a7e50d939d2a815d7
ville pietikäinen <rvpietikainen@gmail.com>
2020-10-24 11:09:36 +0300

    initial commit

---
+++ fsk2csv/FskModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fsk2csv
+{
+    public class FskModel
+    {
+        public FskEntityContainer data { get; set; }
+    }
+
+    public class FskEntityContainer : Dictionary<string, FskEntity>
+    {
+    }
+
+    public class FskPasswordHistory
+    {
+        public string password { get; set; }
+        public double changedata { get; set; }
+    }
+
+    public class FskEntity
+    {
+        public string rev { get; set; }
+        public string service { get; set; }
+        public string url { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string notes { get; set; }
+        public string color { get; set; }
+        public string style { get; set; }
+        public int type { get; set; }
+        public List<FskPasswordHistory> passwordList { get; set; }
+    }
+}
---
+++ fsk2csv/Program.cs
@@ -0,0 +1,35 @@
+﻿using Nancy.Json;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace fsk2csv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fskfilename = args[0];
+            string outputfile = args[0] + ".csv";
+
+            Console.WriteLine($"reading file {fskfilename}...");
+
+            var filedata = File.ReadAllText(fskfilename);
+
+            var model = JsonConvert.DeserializeObject<FskModel>(filedata);
+
+            Console.WriteLine(model);
+
+            File.WriteAllText(outputfile, "rev;service;url;username;password;notes;color;style;type;passwordlist\r\n");
+
+            foreach (var key in model.data.Keys)
+            {
+                var entity = model.data[key];
+                Console.WriteLine($"entity {key}: {entity.service}");
+
+                File.AppendAllText(outputfile, $"\"{entity.rev}\";\"{entity.service}\";\"{entity.url}\";\"{entity.username}\";\"{entity.password}\";\"{entity.notes}\";\"{entity.color}\";\"{entity.style}\";\"{entity.type}\";\"\"\r\n");
+            }
+        }
+    }
+}
